dc2a2b408102acd1fb81e650a4ed62f6327b1e65
Mrklod <osinsasha364@gmail.com>
2023-07-31 17:20:43 +0300

    1

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import *
 
-
 app_name = 'blog'
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
-    path('<int:id>/',post_detail, name='post_detail')
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         post_detail,
+         name='post_detail'),
+    path('<int:post_id>/share/',
+         post_share, name='post_share'),
+    path('<int:post_id>/comment/',
+         post_comment, name='post_comment'),
 ]
